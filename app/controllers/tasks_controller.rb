8ebbcf13e6020cc743006953fa92b33fca5ee50e
akihiro.nakajima <akihiro-nakajima@solar-partners.co.jp>
2022-03-29 08:15:03 +0000

    modify tasks#index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
   
   def show
@@ -43,15 +42,11 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = "Task は正常に削除されました"
-    redirect_to tasks_url
+    redirect_to root_url
   end
 
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
+
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
